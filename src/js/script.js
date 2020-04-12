ff782e537dce60233f7405003c0e86b5b49e9617
Anton Lukyanov <lukyanov29.05.93@yandex.ru>
2020-04-12 12:49:25 +0700

    slider done

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,20 @@
-const qq = 123;
+import Glide from '@glidejs/glide'
 
-export {qq};
+const config = {
+  type: 'carousel',
+  startAt: 0,
+  perView: 3.5,
+  breakpoints: {
+    1024: {
+      perView: 2.5
+    },
+    700: {
+      perView: 1.5
+    },
+    460: {
+      perView: 1.06
+    }
+  }
+};
+
+new Glide('.glide', config).mount()
